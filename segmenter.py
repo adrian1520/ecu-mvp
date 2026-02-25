98d79c91d2579c3bcae549bcdab3f9ca56df09fb
adrian1520 <adrian.adamowicz.x@gmail.com>
2026-02-25 06:09:39 +0100

    Delete segmenter.py

--- segmenter.py
+++
@@ -1,4 +0,0 @@
-def find_calibration_region(data: bytes):
-    start = next(i for i, b in enumerate(data) if b != 0xFF)
-    end = len(data) - next(i for i, b in enumerate(reversed(data)) if b != 0xFF)
-    return start, end
